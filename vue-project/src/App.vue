f108ea918e0b7ff1252d14575af3f41092bbb116
Joshua Concepcion <jtconcepcion@student.apc.edu.ph>
2025-02-26 08:21:30 +0000

    Added Supabase

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,31 @@
-<script></script>
-
 <template>
-  <h1>Hello world!</h>
+  <h1>Food</h1>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+  <br>
+  <personal-profile/> <br>
+  <food-item/>
+  <food-item2/>
+  <food-item/>
 </template>
 
-<style></style>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
+
+<style>
+</style>
+
---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://ctimdtuftdabdvjfmfri.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0aW1kdHVmdGRhYmR2amZtZnJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NTY0OTQsImV4cCI6MjA1NjEzMjQ5NH0.q_Gz88srH4xiupRu_HgDrdrTG4FckfIcB-_xBteefok')
